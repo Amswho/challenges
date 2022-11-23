17635dd5c5a5a23b78778ceedb36755f9fd9eec8
Amswho <116364806+Amswho@users.noreply.github.com>
2022-11-22 22:56:54 -0500

    Challenge day 2, Amy

---
+++ intermediate/days/1/amy/amy.py
@@ -0,0 +1,10 @@
+
+pas = ['fads5464gfgfdg4fd5s6474h4gffk44fdh4sfv14fn4m487iodr754hdf2g1','hj87mkff4s5d64787uytk4gh41n3dsgapnl4kjh78yu412gkfhj4f578iyt',
+       'rqups45sdj7m43xby7874kghg4g7iupl4zcv4fad448784asfdf']
+
+for count in pas:
+    len = 0
+    for i in count: len += 1
+    print(len)
+    
+
